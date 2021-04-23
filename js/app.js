b52ec272109dfec40e073ff160faf0898afd9f99
Tdunc <Tyson.121@hotmail.com>
2021-04-23 00:47:41 -0600

    its 1am

---
+++ js/app.js
@@ -0,0 +1,20 @@
+function _class(name){
+  return document.getElementsByClassName(name);
+}
+ 
+let tabPanes = _class("tab-header")[0].getElementsByTagName("div");
+ 
+for(let i=0;i<tabPanes.length;i++){
+  tabPanes[i].addEventListener("click",function(){
+    _class("tab-header")[0].getElementsByClassName("active-tab")[0].classList.remove("active-tab");
+    tabPanes[i].classList.add("active-tab");
+    
+    _class("tab-indicator")[0].style.top = `calc(80px + ${i*45}px)`;
+    
+    _class("tab-content")[0].getElementsByClassName("active-tab")[0].classList.remove("active-tab");
+    _class("tab-content")[0].getElementsByTagName("div")[i].classList.add("active-tab");
+    
+  });
+}
+
+
